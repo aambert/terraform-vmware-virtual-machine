ed095e72dd70771bb46c5a61e2b6c37b6e0c865e
Kevin Head <kevin.head@wellsky.com>
2019-05-22 08:41:39 -0500

    fixed missing resource and unsupported attrib

--- output.tf
+++ output.tf
@@ -1,14 +1,30 @@
 output "virtual_machine_names" {
-  value       = "${vsphere_virtual_machine.vm.*.name}"
+  value = ["${flatten(list(
+    vsphere_virtual_machine.bare.*.name,
+    vsphere_virtual_machine.linux.*.name,
+    vsphere_virtual_machine.windows.*.name,
+  ))}"]
   description = "List of virtual machine names."
 }
 
 output "virtual_machine_addresses" {
-  value       = "${vsphere_virtual_machine.vm.*.default_ip_address}"
+  value = ["${flatten(list(
+    vsphere_virtual_machine.bare.*.default_ip_address,
+    vsphere_virtual_machine.linux.*.default_ip_address,
+    vsphere_virtual_machine.windows.*.default_ip_address,
+  ))}"]
   description = "List of virtual machine IPs."
 }
 
 output "virtual_machine_name_address_map" {
-  value       = "${zipmap(vsphere_virtual_machine.vm.*.name, vsphere_virtual_machine.vm.*.default_ip_address)}"
+  value = "${zipmap(flatten(list(
+    vsphere_virtual_machine.bare.*.name,
+    vsphere_virtual_machine.linux.*.name,
+    vsphere_virtual_machine.windows.*.name,
+    )), flatten(list(
+    vsphere_virtual_machine.bare.*.default_ip_address,
+    vsphere_virtual_machine.linux.*.default_ip_address,
+    vsphere_virtual_machine.windows.*.default_ip_address,
+  )))}"
   description = "Map of virtual machine name w/ default ip address. Example: vm1 = 172.0.0.2, vm2 = 172.0.0.3"
 }
--- vsphere_virtual_machine_bare.tf
+++ vsphere_virtual_machine_bare.tf
@@ -2,7 +2,7 @@ resource "vsphere_virtual_machine" "bare" {
   count = "${var.virtual_machine_template_os_family == "" ? var.virtual_machine_instance_count : 0}"
 
   name             = "${format("%s-%d", var.virtual_machine_name_prefix, count.index)}"
-  resource_pool_id = "${var.resource_pool != "" ? data.vsphere_resource_pool.resource_pool.id : data.vsphere_compute_cluster.cluster.resource_pool.id}"
+  resource_pool_id = "${var.resource_pool != "" ? data.vsphere_resource_pool.resource_pool.id : data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
   annotation = "Managed by Terraform"
